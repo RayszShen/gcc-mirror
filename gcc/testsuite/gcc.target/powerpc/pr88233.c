d34a3eb9286d547533a7226a504c229b2ab6d4b3
Alexandre Oliva <oliva@adacore.com>
2024-05-29 02:52:14 -0300

    add explicit ABI and align options to pr88233.c
    We've observed failures of this test on powerpc configurations that
    default to different calling conventions and alignment requirements.
    Both settings are needed for the original expectations to be met.
    
    The test was later modified to have different expectations for big and
    little endian code generation.  This patch restores the original
    codegen expectations, that, with the explicit options, don't vary any
    more.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.target/powerpc/pr88233.c: Make some alignment strictness
    	and calling conventions assumptions explicit.  Restore uniform
    	codegen expectations

--- gcc/testsuite/gcc.target/powerpc/pr88233.c
+++ gcc/testsuite/gcc.target/powerpc/pr88233.c
@@ -1,5 +1,5 @@
 /* { dg-require-effective-target lp64 } */
-/* { dg-options "-O2 -mdejagnu-cpu=power8" } */
+/* { dg-options "-O2 -mdejagnu-cpu=power8 -mno-strict-align -fpcc-struct-return" } */
 
 typedef struct { double a[2]; } A;
 A
@@ -9,6 +9,5 @@ foo (const A *a)
 }
 
 /* { dg-final { scan-assembler-not {\mmtvsr} } } */
-/* { dg-final { scan-assembler-times {\mlxvd2x\M} 1 { target { be } } } } */
-/* { dg-final { scan-assembler-times {\mstxvd2x\M} 1 { target { be } } } } */
-/* { dg-final { scan-assembler-times {\mlfd\M} 2 { target { le } } } } */
+/* { dg-final { scan-assembler-times {\mlxvd2x\M} 1 } } */
+/* { dg-final { scan-assembler-times {\mstxvd2x\M} 1 } } */
